73e02aecedb16e3b0e786ae6e6d6d6b263c43acc
Sl0cx <simon.maniez.creation@gmail.com>
2023-06-04 22:25:30 +0200

    :sparkles: :construction: Add SSO & User Component.

--- App.tsx
+++ App.tsx
@@ -3,10 +3,9 @@ import AppNavigator from './src/components/AppNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import DangerScreen from './src/screens/DangerScreen';
-// import { useStore } from "./src/hooks/useStore";
 import { ApplicationProvider } from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
-import axios from "axios";
+import SSO from './src/components/SSO'; // Update the import statement
 
 interface State {
   randomAlbumId: string;
@@ -14,18 +13,20 @@ interface State {
 
 const Stack = createStackNavigator();
 
+
 class App extends React.Component<{}, State> {
-  // setAuthData = useStore((state: any) => state.setAuthData);
+
   render() {
     return (
       <ApplicationProvider {...eva} theme={eva.light}>
-      <NavigationContainer>
-        <Stack.Navigator headerMode='none'>
-          <Stack.Screen name='App' component={AppNavigator} />
-          <Stack.Screen name='DangerScreen' component={DangerScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </ApplicationProvider>
+        <NavigationContainer>
+          <Stack.Navigator headerMode='none'>
+            {/* <Stack.Screen name='App' component={AppNavigator} />
+            <Stack.Screen name='DangerScreen' component={DangerScreen} /> */}
+            <Stack.Screen name='SSO' component={SSO} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ApplicationProvider>
     );    
   }
 }
