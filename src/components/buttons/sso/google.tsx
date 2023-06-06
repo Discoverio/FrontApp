d169fd806df7ed063d2affc0a6b596fb3f746fdf
Sl0cx <simon.maniez.creation@gmail.com>
2023-06-06 18:53:25 +0200

    :lock: Security of Variables.

--- src/components/buttons/sso/google.tsx
+++ src/components/buttons/sso/google.tsx
@@ -2,6 +2,7 @@ import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import { useStore } from "../../../hooks/useStore";
 import axios from "axios";
 import React from "react";
+import Constants from 'expo-constants';
 
 interface Props {
   onLogin: () => void;
@@ -10,14 +11,13 @@ interface Props {
 const GoogleLoginWidget: React.FC<Props> = ({ onLogin }) => {
   const setAuthData = useStore((state: any) => state.setAuthData);
   // Get the client ID from environment variable or use a default value
-  // const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID || "";
-  const clientId = "674546016382-4b1st85hr20iv3ckr1mdrtt0itrvrg7n.apps.googleusercontent.com";
-
+  const clientId = Constants.manifest?.extra?.REACT_APP_GOOGLE_CLIENT_ID || '';
+  const backend_adress = Constants.manifest?.extra?.REACT_APP_BACKEND_ADRESS || '';
   const handleSuccess = async (credentialResponse: any) => {
     console.log(credentialResponse);
     try {
       const { data } = await axios.post(
-        "http://localhost:3000/login",
+        backend_adress,
         {
           // pass the token as part of the req body
           token: credentialResponse.credential,
